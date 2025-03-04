e8852c07826a4dc49df2911eff2936d645cbed3b
Eyob Berhanu <all.eloquent362@passinbox.com>
2025-03-04 14:47:42 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,55 @@
 const body = document.body;
 const image = body.querySelector('#coin');
-const h1 = body.querySelector('h1');
+const h1 = body.querySelector('#balance');
 
 let coins = localStorage.getItem('coins');
 let total = localStorage.getItem('total');
 let power = localStorage.getItem('power');
-let count = localStorage.getItem('count')
+let count = localStorage.getItem('count');
 
-if(coins == null){
-    localStorage.setItem('coins' , '0');
+// Initialize coins if not already set
+if (coins === null) {
+    localStorage.setItem('coins', '0');
     h1.textContent = '0';
-}else{
+} else {
     h1.textContent = Number(coins).toLocaleString();
 }
 
-if(total == null){
-    localStorage.setItem('total' , '500')
+// Check if total and power values exist. If not, initialize them to 500
+if (total === null) {
+    localStorage.setItem('total', '500');
     body.querySelector('#total').textContent = '/500';
-}else {
+} else {
     body.querySelector('#total').textContent = `/${total}`;
 }
 
-
-if(power == null){
-    localStorage.setItem('power' , '500');
+if (power === null) {
+    localStorage.setItem('power', '500');
     body.querySelector('#power').textContent = '500';
-}else{
+} else {
     body.querySelector('#power').textContent = power;
 }
 
-
-if(count == null){
-    localStorage.setItem('count' , '1')
+if (count === null) {
+    localStorage.setItem('count', '1')
 }
 
-image.addEventListener('click' , (e)=> {
-
+image.addEventListener('click', (e)=> {
     let x = e.offsetX;
     let y = e.offsetY;
-
 
     navigator.vibrate(5);
 
     coins = localStorage.getItem('coins');
     power = localStorage.getItem('power');
-    
+
     if(Number(power) > 0){
         localStorage.setItem('coins' , `${Number(coins) + 1}`);
         h1.textContent = `${(Number(coins) + 1).toLocaleString()}`;
-    
+
         localStorage.setItem('power' , `${Number(power) - 1}`);
         body.querySelector('#power').textContent = `${Number(power) - 1}`;
-    } 
+    }
 
     if(x < 150 & y < 150){
         image.style.transform = 'translate(-0.25rem, -0.25rem) skewY(-10deg) skewX(5deg)';
@@ -66,11 +64,9 @@ image.addEventListener('click' , (e)=> {
         image.style.transform = 'translate(0.25rem, -0.25rem) skewY(10deg) skewX(-5deg)';
     }
 
-
     setTimeout(()=>{
         image.style.transform = 'translate(0px, 0px)';
     }, 100);
 
     body.querySelector('.progress').style.width = `${(100 * power) / total}%`;
 });
-
